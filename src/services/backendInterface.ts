91c8987d4a38a390aa599b2242f977fefaee9097
jack272hughes <jack.hughes2@sky.uk>
2022-05-29 17:42:18 +0100

    Update backend interface to use function type

--- src/services/backendInterface.ts
+++ src/services/backendInterface.ts
@@ -1,45 +1,43 @@
 function generateRandomString(): String {
-    return Math.random().toString(36).substring(2);
+	return Math.random().toString(36).substring(2);
 }
 
 function request(): Promise<String[]> {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            const stringArray: String[] = [];
-            for (let i = 0; i < 5; i++) {
-                stringArray.push(generateRandomString());
-            }
-            resolve(stringArray);
-        })
-    })
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			const stringArray: String[] = [];
+			for (let i = 0; i < 5; i++) {
+				stringArray.push(generateRandomString());
+			}
+			resolve(stringArray);
+		});
+	});
 }
 
-interface Subscription {
-    update: (word: String) => void
-}
+type Subscription = (word: String) => void;
 
 const subscriptions: Subscription[] = [];
 function subscribe(subscription: Subscription) {
-    subscriptions.push(subscription);
+	subscriptions.push(subscription);
 }
 
 function updateSubscribers() {
-    const word: String = generateRandomString()
-    subscriptions.forEach(subscription => subscription.update(word))
+	const word: String = generateRandomString();
+	subscriptions.forEach((func) => func(word));
 }
 
-let intervalId: NodeJS.Timer
+let intervalId: NodeJS.Timer;
 let subscriptionActive = false;
 function startSubscription(): void {
-    if (!subscriptionActive) {
-        intervalId = setInterval(updateSubscribers, 1000);
-        subscriptionActive = true;
-    }
+	if (!subscriptionActive) {
+		intervalId = setInterval(updateSubscribers, 1000);
+		subscriptionActive = true;
+	}
 }
 
 function stopSubscription(): void {
-    clearInterval(intervalId)
-    subscriptionActive = false;
+	clearInterval(intervalId);
+	subscriptionActive = false;
 }
 
-export { request, subscribe, startSubscription, stopSubscription }
+export { request, subscribe, startSubscription, stopSubscription };
